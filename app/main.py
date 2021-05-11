4c1265361539a5ef1bf5851f36fdbd3a9bcdbd12
Sinjin Jang <sinjin@stronghold.kr>
2021-05-11 11:45:10 +0900

    백그라운드 작업 API 추가

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, BackgroundTasks
 
 app = FastAPI()
 
@@ -6,3 +6,13 @@ app = FastAPI()
 @app.get('/')
 def read_root():
     return {'Hello': 'World'}
+
+
+def run_shell_cmd(email: str):
+    print('run_ext_process_task', email)
+
+
+@app.get('/run-bgtasks/{email}')
+async def run_bgtasks(email: str, bg_tasks: BackgroundTasks):
+    bg_tasks.add_task(run_shell_cmd, email)
+    return {'result': 'Background job is in queue'}
