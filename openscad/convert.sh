025e95e4d6d6e2d22972d96aee43fb652f7761bb
Sinjin Jang <sinjin@stronghold.kr>
2021-05-11 15:30:12 +0900

    AppImage를 압축해제하여 실행하도록 변경

--- openscad/convert.sh
+++ openscad/convert.sh
@@ -2,13 +2,15 @@
 
 SCAD_FILE=$1
 
-OPENSCAD_APP=OpenSCAD-2021.01-x86_64.AppImage
-if [ ! -f ${OPENSCAD_APP} ]; then
-    wget https://files.openscad.org/${OPENSCAD_APP}
-    chmod a+x ${OPENSCAD_APP}
-    ln -sf ${OPENSCAD_APP} openscad
+OPENSCAD_EXECUTABLE=./squashfs-root/AppRun
+if [ ! -f ${OPENSCAD_EXECUTABLE} ]; then
+    OPENSCAD_APPIMAGE=OpenSCAD-2021.01-x86_64.AppImage
+    wget https://files.openscad.org/${OPENSCAD_APPIMAGE}
+    chmod a+x ${OPENSCAD_APPIMAGE}
+    ./${OPENSCAD_APPIMAGE} --appimage-extract
+    rm ./${OPENSCAD_APPIMAGE}
 fi
 
-./openscad --export-format stl -o output.stl ${SCAD_FILE}
+${OPENSCAD_EXECUTABLE} --export-format stl -o output.stl ${SCAD_FILE}
 
 echo 'converting done!!!'
